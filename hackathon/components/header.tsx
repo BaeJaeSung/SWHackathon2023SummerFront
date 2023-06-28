40e93b7faba0f69a9e98a15c43669064e0c2d0e2
dayongkr <dayongkr@gmail.com>
2023-06-29 00:19:33 +0900

    center

--- hackathon/components/header.tsx
+++ hackathon/components/header.tsx
@@ -3,9 +3,9 @@ import Icon from '@/public/notifications_FILL1_wght400_GRAD0_opsz48.svg'
 export default function Header() {
   return (
     <nav className="flex h-16 w-full items-center justify-between p-5">
-      <div />
+      <div className='w-8' />
       <h1 className="text-xl font-semibold">빛나는 연결</h1>
-      <Image width={30} height={30} src={Icon} alt={'notification'} className='opacity-50 hover:opacity-80 cursor-pointer' />
+      <Image width={32} height={32} src={Icon} alt={'notification'} className='opacity-50 hover:opacity-80 cursor-pointer' />
     </nav>
   )
 }
