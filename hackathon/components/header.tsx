c19c039387d80cef730a4f2709d7e3fcdc703fe6
dayongkr <dayongkr@gmail.com>
2023-06-29 00:11:04 +0900

    notification2

--- hackathon/components/header.tsx
+++ hackathon/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <nav className="flex h-16 w-full items-center justify-between p-5">
       <div />
       <h1 className="text-xl font-semibold">빛나는 연결</h1>
-      <Image width={30} height={30} src={Icon} alt={'notification'} className='opacity-50' />
+      <Image width={30} height={30} src={Icon} alt={'notification'} className='opacity-50 hover:opacity-80 cursor-pointer' />
     </nav>
   )
 }
