274d874a2bb1aa051ddd6374d89caddfb64a94ce
dayongkr <dayongkr@gmail.com>
2023-06-29 00:10:44 +0900

    notification

--- hackathon/components/header.tsx
+++ hackathon/components/header.tsx
@@ -1,9 +1,11 @@
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import Image from 'next/image'
+import Icon from '@/public/notifications_FILL1_wght400_GRAD0_opsz48.svg'
 export default function Header() {
   return (
-    <nav className="w-full h-16 flex justify-center items-center">
-      <h1 className="font-semibold text-xl">빛나는 연결</h1>
-      <NotificationsIcon/>
+    <nav className="flex h-16 w-full items-center justify-between p-5">
+      <div />
+      <h1 className="text-xl font-semibold">빛나는 연결</h1>
+      <Image width={30} height={30} src={Icon} alt={'notification'} className='opacity-50' />
     </nav>
   )
 }
