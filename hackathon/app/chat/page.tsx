62bf839a024cd04104b2b63887e58371510e89b7
dayongkr <dayongkr@gmail.com>
2023-06-29 20:27:09 +0900

    chat

--- hackathon/app/chat/page.tsx
+++ hackathon/app/chat/page.tsx
@@ -1,6 +1,10 @@
 'use client'
 import Image from 'next/image'
+import Logo from '@/public/logo.png'
 import Dummy from '@/public/dummy.png'
+import Green from '@/public/green.svg'
+import Purple from '@/public/purple.svg'
+import LeftIcon from '@/public/chevron_left_FILL0_wght400_GRAD0_opsz48.svg'
 import { useState } from 'react'
 
 export default function ChatPage() {
@@ -92,8 +96,58 @@ export default function ChatPage() {
       <div
         className={`absolute bottom-0 left-0 right-0 top-0 z-50 ${
           !visible && 'translate-x-full'
-        } bg-[#00396E] ease-in-out`}
-      ></div>
+        } flex flex-col justify-between bg-[#00396E] transition-transform duration-500 ease-in-out`}
+      >
+        <nav className="relative flex h-24 w-full flex-col items-center justify-center gap-2">
+          <Image src={Logo} width={35} height={35} alt="logo" />
+          <p className="text-lg font-bold text-white">한국카페</p>
+
+          <Image
+            src={LeftIcon}
+            width={40}
+            height={40}
+            alt="left"
+            className="absolute left-5 cursor-pointer"
+            onClick={() => {
+              setVisible(!visible)
+            }}
+          />
+        </nav>
+        <div className="h-full overflow-y-auto p-5">
+          <div className="chat chat-start">
+            <div className="chat-bubble relative bg-white p-4 text-black">
+              <Image
+                src={Green}
+                width={35}
+                height={35}
+                alt="green"
+                className="absolute -top-4 left-2"
+              />
+              안녕하세요
+            </div>
+            <div className="chat-footer mt-2 text-xs text-white">12:41</div>
+          </div>
+          <div className="chat chat-end relative">
+            <div className="chat-bubble relative bg-white p-4 text-black">
+              <Image
+                src={Purple}
+                width={35}
+                height={35}
+                alt="green"
+                className="absolute -top-4 right-2"
+              />
+              안녕하세요 이래영이라고 합니다.
+            </div>
+            <div className="chat-footer mt-2 text-xs text-white">12:45</div>
+          </div>
+        </div>
+        <div className="flex h-24 items-center justify-between gap-3 rounded-t-2xl bg-[#ECEFF4] px-5">
+          <input type="text" className="input grow rounded-full bg-white" />
+          <button className="w-16 rounded-2xl bg-[#C467EF] px-3 py-2 text-white">
+            전송
+          </button>
+        </div>
+      </div>
     </>
   )
 }
