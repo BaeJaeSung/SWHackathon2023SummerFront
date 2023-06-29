87adc2ce59dab9e3a028c56b9a2801b3a6f9539b
dayongkr <dayongkr@gmail.com>
2023-06-29 09:47:16 +0900

    session

--- hackathon/app/layout.tsx
+++ hackathon/app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from '@/components/footer'
 import './globals.css'
 import Header from '@/components/header'
 import { Noto_Sans } from 'next/font/google'
+import { getSession } from '@/lib/api'
 
 const noto = Noto_Sans({ subsets: ['latin'], weight: ['400', '600', '700'] })
 
@@ -10,17 +11,18 @@ export const metadata = {
   description: '2023',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getSession()
   return (
     <html lang="en">
       <body className={noto.className}>
         <div className="flex h-screen w-full flex-wrap items-center justify-center">
           <div className="bg-gray-10 flex h-full w-full max-w-lg flex-col justify-between bg-[#EFEFED]">
-            <Header />
+            {session && <Header />}
             <main className="h-full w-full">{children}</main>
             {/* <Footer /> */}
           </div>
