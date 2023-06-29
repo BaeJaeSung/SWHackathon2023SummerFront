18991e7de9481394a13685a740dc4ed7731523b9
dayongkr <dayongkr@gmail.com>
2023-06-30 02:26:10 +0900

    header text xl

--- hackathon/components/header.tsx
+++ hackathon/components/header.tsx
@@ -9,7 +9,7 @@ export default async function Header() {
       <nav className="flex h-24 w-full items-center pl-5">
         <Link href="/" className="flex gap-3">
           <Image src={Logo} width={30} height={30} alt="빛나는 연결" />
-          <span className="flex items-center justify-center text-lg font-bold text-[#00396E]">
+          <span className="flex items-center justify-center text-xl font-bold text-[#00396E]">
             빛나는 연결
           </span>
         </Link>
