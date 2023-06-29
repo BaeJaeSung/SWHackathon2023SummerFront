2ee6dd0b451fa4f22e9124f66083b500587bf551
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-06-29 10:28:21 +0900

    second 10 28

--- hackathon/app/signup/new/detail/page.tsx
+++ hackathon/app/signup/new/detail/page.tsx
@@ -1,3 +1,81 @@
+"use client"
+import SignUpLayout from "@/components/signUpLayout"
+import { useRouter } from "next/navigation"
+
 export default function SignUpDetailPage() {
-  return <div>detail</div>
+  const router = useRouter()
+  const onClick = () => {
+    router.push('/');
+  }
+  return (
+    <SignUpLayout
+      textTop="00님의"
+      textBottom="자기소개가 궁금해요!"
+      btnText="다음"
+      btnEvent={onClick}
+    >
+      <div className="flex flex-col gap-5">
+        <div>
+          <label htmlFor="detail" className="cursor-pointer">
+            나의 정보
+          </label>
+          <input
+            id="detail"
+            type="text"
+            className="bg-white-200 input mt-2 w-full "
+          />
+        </div>
+        <div>
+          <label htmlFor="edu" className="cursor-pointer" className="label">
+            {/*<span className="label-text"></span>*/}학력
+          </label>
+          <select id="edu" className="select w-full max-w-xs">
+            <option selected>대졸</option>
+            <option>초졸</option>
+            <option>중졸</option>
+            <option>고졸</option>
+          </select>
+        </div>
+        <div>
+          <div className="mb-3">아르바이트 근무 경험이 있다면?</div>
+          <div>
+            <label htmlFor="company" className="cursor-pointer">
+              회사명
+            </label>
+            <input
+              id="company"
+              type="text"
+              className="bg-white-200 input mt-2 w-full"
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor="period" className="cursor-pointer" className="label">
+            {/*<span className="label-text"></span>*/}근무 기간
+          </label>
+          <select id="period" className="select w-full max-w-xs">
+            <option selected>3일 이하</option>
+            <option>1주일</option>
+            <option>1개월</option>
+            <option>3개월</option>
+            <option>6개월</option>
+            <option>1년</option>
+            <option>1년 이상</option>
+          </select>
+        </div>
+        <div>
+          <div>
+            <label htmlFor="exp" className="cursor-pointer">
+              근무를 통한 나의 경험
+            </label>
+            <input
+              id="exp"
+              type="text"
+              className="bg-white-200 input mt-2 w-full"
+            />
+          </div>
+        </div>
+      </div>
+    </SignUpLayout>
+  )
 }
--- hackathon/app/signup/page.tsx
+++ hackathon/app/signup/page.tsx
@@ -1,6 +1,9 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import SignUpLayout from '@/components/signUpLayout'
+import ICON_NEW from '@/public/new.png'
+import ICON_OLD from '@/public/old.png'
+import Image from 'next/image'
 
 export default function SignUpPage() {
   const router = useRouter()
@@ -14,12 +17,14 @@ export default function SignUpPage() {
       btnText="다음"
       btnEvent={onClick}
     >
-      <div className="flexx flex-col gap-5">
-        <div className="mb-5 rounded-md bg-gray-300 p-24 text-center">
-          청년이예요
+      <div className="flex flex-col gap-5">
+        <div className="flex h-60 cursor-pointer flex-col justify-between rounded-md bg-[#8F00FF] p-10">
+          <Image src={ICON_NEW} width={48} height={48} alt="청년" />
+          <div className="text-2xl font-semibold text-white">청년이에요</div>
         </div>
-        <div className="text rounded-md bg-gray-300 p-24 text-center">
-          사장님이예요
+        <div className="flex h-60 cursor-pointer flex-col justify-between rounded-md bg-[#35FF6E] p-10">
+          <Image src={ICON_OLD} width={48} height={48} alt="자영업자" />
+          <div className="text-2xl font-semibold">사장님이에요</div>
         </div>
       </div>
     </SignUpLayout>
