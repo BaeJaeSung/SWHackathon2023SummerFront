2d1864f0092630c1207e96632ffa22b5380e7c51
dayongkr <dayongkr@gmail.com>
2023-06-30 03:09:59 +0900

    footer update

--- hackathon/components/footer.tsx
+++ hackathon/components/footer.tsx
@@ -1,54 +1,63 @@
 'use client'
-import { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation'
 import Image from 'next/image'
 import LOGO_FOOTER from '@/public/old.svg'
-import HEART from '@/public/heart.svg'
+import HEART from '@/public/thumb.svg'
 import MESSAGE from '@/public/mess.svg'
 import MY from '@/public/my.svg'
 import LOGO_FOOTER_AC from '@/public/old_ac.svg'
-import HEART_AC from '@/public/heart_ac.svg'
+import HEART_AC from '@/public/thumb_ac.svg'
 import MESSAGE_AC from '@/public/mess_ac.svg'
 import MY_AC from '@/public/my_ac.svg'
 import Link from 'next/link'
 
-export default function Header() {
+export default function Footer() {
+  const path = usePathname()
+  console.log(path)
   return (
     <nav className="flex h-16 w-full items-center justify-around p-5">
       <Link href="/">
         <Image
           width={45}
           height={45}
-          src={LOGO_FOOTER}
+          src={path == '/' ? LOGO_FOOTER_AC : LOGO_FOOTER}
           alt={'logo'}
-          className={`hover: cursor-pointer opacity-20 hover:opacity-50`}
+          className={`hover: cursor-pointer ${
+            path == '/' || 'opacity-20 hover:opacity-50'
+          }`}
         />
       </Link>
       <Link href="heart">
         <Image
           width={35}
           height={35}
-          src={HEART}
-          alt={'heart'}
-          className="cursor-pointer
-          opacity-20 hover:opacity-50"
+          src={path == '/heart' ? HEART_AC : HEART}
+          alt={'logo'}
+          className={`hover: cursor-pointer ${
+            path == '/heart' || 'opacity-20 hover:opacity-50'
+          }`}
         />
       </Link>
       <Link href="chat">
         <Image
           width={35}
           height={35}
-          src={MESSAGE}
-          alt={'message'}
-          className="cursor-pointer opacity-20  hover:opacity-50"
+          src={path == '/chat' ? MESSAGE_AC : MESSAGE}
+          alt={'chat'}
+          className={`hover: cursor-pointer ${
+            path == '/chat' || 'opacity-20 hover:opacity-50'
+          }`}
         />
       </Link>
       <Link href="info">
         <Image
-          width={45}
-          height={45}
-          src={MY}
-          alt={'info'}
-          className="cursor-pointer opacity-20  hover:opacity-50"
+          width={35}
+          height={35}
+          src={path == '/info' ? MY_AC : MY}
+          alt={'INFO'}
+          className={`hover: cursor-pointer ${
+            path == '/info' || 'opacity-20 hover:opacity-50'
+          }`}
         />
       </Link>
     </nav>
