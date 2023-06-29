eac557edfdc14525e82ecb6368b5e8ab33257fb4
dayongkr <dayongkr@gmail.com>
2023-06-29 22:28:31 +0900

    sign

--- hackathon/app/api/auth/[...nextauth]/route.js
+++ hackathon/app/api/auth/[...nextauth]/route.js
@@ -12,21 +12,34 @@ export const authOption = {
         id: { label: 'Id', type: 'id' },
         password: { label: 'Password', type: 'password' },
       },
-      authorize: async (credentials) => {
+      authorize: async (credentials, req) => {
         const res = await axios.post('http://3.39.72.59:3000/user/login', {
           id: credentials?.id,
           pw: credentials?.password,
         })
-        console.log(res.data)
-        if (res.data) {
-          return { id: credentials?.id }
+        console.log(res.data.success)
+        if (res.data.success) {
+          return { id: credentials.id }
         }
         return null
       },
     }),
   ],
   secret: 'test',
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      session.user.id = token.id
+      return session
+    },
+  },
 }
-
 const handler = NextAuth(authOption)
 export { handler as GET, handler as POST }
